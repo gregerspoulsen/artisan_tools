71c0656eaa7b430245eb37de1a97862f5a4dc23d
Gregers Poulsen <gp@airborneinstruments.dk>
2023-12-13 10:12:52 +0100

    Add black formatter

--- src/artisan_tools/version.py
+++ src/artisan_tools/version.py
@@ -4,6 +4,7 @@ Tools to support release of a package.
 import os
 import semver
 
+
 def bump_version(version, part):
     """
     Bump the specified part (major, minor, or patch) of a semantic version string.
@@ -18,14 +19,15 @@ def bump_version(version, part):
     Raises:
     ValueError: If the part is not 'major', 'minor', or 'patch'.
     """
-    if part == 'major':
+    if part == "major":
         return semver.bump_major(version)
-    elif part == 'minor':
+    elif part == "minor":
         return semver.bump_minor(version)
-    elif part == 'patch':
+    elif part == "patch":
         return semver.bump_patch(version)
     else:
         raise ValueError("Invalid part to bump: must be 'major', 'minor', or 'patch'")
+
 
 def bump_version_file(file_path, part):
     """
@@ -40,17 +42,18 @@ def bump_version_file(file_path, part):
         raise FileNotFoundError(f"File not found: {file_path}")
 
     # Read the version from the file
-    with open(file_path, 'r') as file:
+    with open(file_path, "r") as file:
         current_version = file.read().strip()
 
     # Bump the version using the bump_version function
     new_version = bump_version(current_version, part)
 
     # Write the new version to the file
-    with open(file_path, 'w') as file:
+    with open(file_path, "w") as file:
         file.write(new_version)
 
     return new_version
+
 
 # Example usage
 # file_path = 'path/to/your/version.txt'  # Replace with your file path
--- tests/artisan_tools/test_version.py
+++ tests/artisan_tools/test_version.py
@@ -1,24 +1,30 @@
 import pytest
-from artisan_tools.version import bump_version, bump_version_file 
+from artisan_tools.version import bump_version, bump_version_file
+
 
 def test_bump_version_major():
     assert bump_version("1.2.3", "major") == "2.0.0"
 
+
 def test_bump_version_minor():
     assert bump_version("1.2.3", "minor") == "1.3.0"
+
 
 def test_bump_version_patch():
     assert bump_version("1.2.3", "patch") == "1.2.4"
 
+
 def test_bump_version_invalid_part():
     with pytest.raises(ValueError):
         bump_version("1.2.3", "invalid_part")
+
 
 def test_bump_version_file(tmpdir):
     temp_file = tmpdir.join("version.txt")
     temp_file.write("1.2.3")
     bump_version_file(str(temp_file), "minor")
     assert temp_file.read() == "1.3.0"
+
 
 def test_bump_version_file_nonexistent(tmpdir):
     nonexistent_file = tmpdir.join("nonexistent_file.txt")
