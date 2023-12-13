6e2fb86d1a114418357b39a879b63f04a13a341a
Gregers Poulsen <gp@airborneinstruments.dk>
2023-12-13 14:31:04 +0100

    Revert "Use actual git repo in tests"
    This reverts commit 0d88ee17ed0f351c5aeda86c5acda192e19e7556.

--- tests/artisan_tools/test_vcs.py
+++ tests/artisan_tools/test_vcs.py
@@ -11,29 +11,49 @@ from artisan_tools.vcs import (
 
 
 class TestGitTagChecker(unittest.TestCase):
+    def test_run_git_command_success(self):
+        with patch("subprocess.check_output") as mock_check_output:
+            mock_check_output.return_value = "v1.0.0\nv2.0.0"
+            result = run_git_command("git tag")
+            self.assertEqual(result, "v1.0.0\nv2.0.0")
+
+    def test_run_git_command_failure(self):
+        with patch(
+            "subprocess.check_output",
+            side_effect=subprocess.CalledProcessError(1, "cmd"),
+        ):
+            with self.assertRaises(subprocess.CalledProcessError):
+                run_git_command("git tag")
 
     def test_get_remote_tags(self):
-        tags = get_remote_tags()
-        self.assertIn('v0.1.0', tags)
+        with patch("subprocess.check_output") as mock_check_output:
+            mock_check_output.return_value = "refs/tags/v1.0.0\nrefs/tags/v2.0.0"
+            tags = get_remote_tags()
+            self.assertIn("v1.0.0", tags)
+            self.assertIn("v2.0.0", tags)
 
     def test_check_tag_exists(self):
-        self.assertTrue(check_tag('v0.1.0'))
+        with patch("subprocess.check_output") as mock_check_output:
+            mock_check_output.return_value = "refs/tags/v1.0.0\nrefs/tags/v2.0.0"
+            self.assertTrue(check_tag("v1.0.0"))
 
     def test_check_tag_does_not_exist(self):
-        self.assertFalse(check_tag('nonexistent-tag'))
+        with patch("subprocess.check_output") as mock_check_output:
+            mock_check_output.return_value = "refs/tags/v1.0.0\nrefs/tags/v2.0.0"
+            self.assertFalse(check_tag("v3.0.0"))
 
-    @patch('artisan_tools.vcs.run_git_command')
+    @patch("artisan_tools.vcs.run_git_command")
     def test_check_current_branch_true(self, mock_run_git_command):
-        mock_run_git_command.return_value = 'main'
-        self.assertTrue(check_current_branch('main'))
+        mock_run_git_command.return_value = "main"
+        self.assertTrue(check_current_branch("main"))
 
-    @patch('artisan_tools.vcs.run_git_command')
+    @patch("artisan_tools.vcs.run_git_command")
     def test_check_current_branch_false(self, mock_run_git_command):
-        mock_run_git_command.return_value = 'develop'
-        self.assertFalse(check_current_branch('main'))
+        mock_run_git_command.return_value = "develop"
+        self.assertFalse(check_current_branch("main"))
 
-    @patch('artisan_tools.vcs.run_git_command')
+    @patch("artisan_tools.vcs.run_git_command")
     def test_check_current_branch_error(self, mock_run_git_command):
-        mock_run_git_command.side_effect = subprocess.CalledProcessError(1, 'cmd')
+        mock_run_git_command.side_effect = subprocess.CalledProcessError(1, "cmd")
         with self.assertRaises(subprocess.CalledProcessError):
-            check_current_branch('any-branch')
+            check_current_branch("any-branch")
