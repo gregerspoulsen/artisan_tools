5c26fcc6e0954c5386bf949665cb8ac899eed3f5
Gregers Poulsen <gp@airborneinstruments.dk>
2024-02-16 13:31:12 +0100

    Fix documentation build on RTD (#21)

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -30,4 +30,4 @@ templates_path = ["_templates"]
 html_theme = "alabaster"
 html_static_path = ["_static"]
 
-version = "0.13.0"
+version = "0.13.1"
--- src/artisan_tools/cli_doc.py
+++ src/artisan_tools/cli_doc.py
@@ -4,6 +4,12 @@ Simple file for exposing click object to sphinx.
 
 import artisan_tools.app
 import typer
+import os
+
+# Create empty artisan.yaml file if it doesn't exist:
+if not os.path.exists("artisan.yaml"):
+    with open("artisan.yaml", "w") as f:
+        f.write("")
 
 app = artisan_tools.app.App()
 app.load_extensions()
