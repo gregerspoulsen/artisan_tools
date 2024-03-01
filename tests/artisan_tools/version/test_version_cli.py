0a164f4ebd99b54e5758f355881c64d117236a00
Gregers Poulsen <gp@airborneinstruments.dk>
2024-03-01 11:49:11 +0100

    Add get to version CLI

--- tests/artisan_tools/version/test_version_cli.py
+++ tests/artisan_tools/version/test_version_cli.py
@@ -18,3 +18,13 @@ def test_bump(tmpdir, app_with_config):
     assert result.exit_code == 0
     version = app.get_extension("version").get_version(app)
     assert version == "1.0.0"
+
+
+def test_get(tmpdir, app_with_config):
+    app = app_with_config
+    result = runner.invoke(factory(app_with_config), ["get"], catch_exceptions=False)
+
+    # Check if the command was successful and output is correct
+    assert result.exit_code == 0
+    version = app.get_extension("version").get_version(app)
+    assert result.output == version
